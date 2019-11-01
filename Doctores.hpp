faa3644d9c49fb6394e79db694210af5eac11880
daniel forero <45154207+danielforerocorredor@users.noreply.github.com>
2019-11-01 09:54:57 -0500

    Add files via upload
    no funciona el import clase Doctores

---
+++ Doctores.hpp
@@ -0,0 +1,20 @@
+#ifndef _Doctores_hpp_
+#define _Doctores_hpp_
+#include "Paciente.hpp"
+#include "citaDia.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Doctores{
+    private:
+        string nombre;
+        string especialidad; 
+        vector<citaDia> disponibilidadSemanal;//vector de longitud 7, que representa los dias de la semana, cada uno tiene un valor booleano que muestra si esta disponible o no.
+    public: 
+        Doctores(string newName, string newEspecialidad);//constructor parametrizado objeto doctores
+        string getName();
+        string getEspecialidad();
+        
+};
+#endif
---
+++ myDoctoresMain.cpp
@@ -0,0 +1,8 @@
+#include "Doctores.hpp"
+
+int main(){
+
+    Doctores Doc1("daniel", "general");
+
+    return 0;
+}
